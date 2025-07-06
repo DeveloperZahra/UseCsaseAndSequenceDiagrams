a3082c9a698e0ee118b2aa4f5af6326a3d91ade0
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-06 09:41:05 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Use_csase_and_Sequence_Diagrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
